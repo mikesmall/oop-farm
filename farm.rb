3dda722db52b2b83b366455e03938d488bace269
mikesmall <michael.l.a.small@gmail.com>
2017-05-29 17:23:45 -0400

    Placeholders added for remaining menu-item methods.

--- farm.rb
+++ farm.rb
@@ -29,18 +29,22 @@ end#call_option
 
 # Menu option 1
 def new_field
+  # You can add fields of choice to your farm by specifying their type (eg. "corn") and their total area in hectares (eg. 100).
 end#new_field
 
 # Menu option 2
 def harvest
+  # When you harvest, you must collect food from every one of your fields and record how much total food you have collected. It should display all of this information.
 end#harvest
 
 # Menu option 3
 def status
+  # You can also check the status of each field, which is a more direct way of observing the status of your fields (their size and type). It should also tell you how much total food you have produced up until this point.
 end#status
 
 # Menu option 4
 def relax
+  # Sometimes a farmer wants to relax. In doing so, you will get a chance to enjoy the beauty of each one of your fields (without harvesting them).
 end#relax
 
 # Menu option 5
--- field.rb
+++ field.rb
@@ -1,12 +1,37 @@
 class Crop
+
   attr_reader :crop_type, :field_hectares, :food_per_hectare
+
   def initialize(crop_type, crop_hectares, food_per_hectare)
     @crop_type = crop_type
     @crop_hectares = field_hectares
     @food_per_hectare = food_per_hectare
   end#initialize
 
-  def harvest
+  # Menu option 1 -- NEW FIELD
+  def self.new_field
+  end#self.new_field
+
+  # Menu option 2 -- HARVEST
+  def self.harvest
     #  When you harvest, you must collect food from every one of your fields and record how much total food you have collected. It should display all of this information.
-  end#harvest
+  end#self.harvest
+
+  # Menu option 3 -- STATUS
+  def self.status
+    # You can also check the status of each field, which is a more direct way of observing the status of your fields (their size and type). It should also tell you how much total food you have produced up until this point.
+  end#self.status
+
+  # Menu option 4 -- RELAX
+  def self.relax
+    # Sometimes a farmer wants to relax. In doing so, you will get a chance to enjoy the beauty of each one of your fields (without harvesting them).
+  end#self.relax
+
+  # Menu option 5 -- EXIT
+  # No additional code needed here.
+
+  
+
+
+
 end#Crop
